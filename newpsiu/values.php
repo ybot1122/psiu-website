f62a4f42e8235f6645770d031ce8927e0e6ad649
ybot1122 <ybot1122@gmail.com>
2014-12-29 19:13:27 -0800

    values updated

--- newpsiu/values.php
+++ newpsiu/values.php
@@ -20,18 +20,13 @@
 			<?= staticContentRender($data[1], false); ?>
 			</div>
 		</div>
+	</div>
+	<!-- right col (picture collage) -->
+	<div class="col-md-6">
 		<!-- row 3 -->
 		<div class="row narrow">
 			<div class="col-md-12">
 			<?= staticContentRender($data[2], false); ?>
-			</div>
-		</div>
-	</div>
-	<!-- right col (picture collage) -->
-	<div class="col-md-6">
-		<div class="row narrow collage">
-			<div class="col-md-12">
-			<!-- TODO: implement function to produce random collage of n pictures -->
 			</div>
 		</div>
 	</div>
